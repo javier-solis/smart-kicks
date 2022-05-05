17b208880bd9490a2a60ceb81d3a6d78b1f63642
Javier Solis <javsolis@mit.edu>
2022-05-05 17:52:00 -0400

    Reshuffled server map files

--- server/get_landmarks.py
+++
@@ -1,7 +0,0 @@
-import json
-
-landmarks_file = '/var/jail/home/team44/landmarks.json'
-
-def request_handler(request):
-    f = open(landmarks_file)
-    return json.dumps(json.load(f))
--- server/map/compute_angle.py
+++ server/map/compute_angle.py
@@ -15,15 +15,3 @@ def request_handler(request):
     forward_azimuth, back_azimuth, dist = geo.inv(lons1=north_lon, lats1=north_lat, lons2=dest_lon, lats2=dest_lat)
 
     return round(90 - forward_azimuth) % 360
-
-
-# lat, lon = 42.355370, -71.100120
-# dest_lat, dest_lon = 42.3649281, -71.0910598
-# geo = Geod(ellps='WGS84')
-
-# forward_azimuth, back_azimuth, dist = geo.inv(lons1=lon, lats1=lat, lons2=dest_lon, lats2=dest_lat)
-# print(forward_azimuth, dist)
-
-# while( (current_angle := input()) != 'QUIT' ):
-#     actual = (forward_azimuth - float(current_angle)) % 360.0
-#     print(f'Actual angle: {actual}')
