be8ce5013ab6c702cd859ffed7d055bca91e474e
wall-e24 <ndiayew@mit.edu>
2022-05-05 03:51:27 -0400

    Update w1_skicks_starter.ino

--- esp32/main/sensor data/w1_skicks_starter.ino
+++ esp32/main/sensor data/w1_skicks_starter.ino
@@ -1,174 +1,3 @@
-// #include <SPI.h>
-// #include <WiFi.h>
-// #include <mpu6050_esp32.h>
-// #include <stdio.h>
-// #include <string.h>
-// #include <Adafruit_BMP280.h>
-// #include <WiFiClientSecure.h>
-// #include <WiFiClient.h>
-// #include <ArduinoJson.h>
-
-// WiFiClientSecure client; 
-// WiFiClientSecure client2;
-
-// #include "extra.h"
-
-// Adafruit_BMP280 bmp; // I2C
-
-
-// float temperature; //variable for temperature
-// float pressure;    //variable for pressure
-// float altitude;    //variable for alititude, but tbh, this is pretty unreliable without calibration
-// const char USER[] = "waly";
-// uint32_t global_time = millis();
-
-// const uint16_t RESPONSE_TIMEOUT = 6000;
-// const uint16_t IN_BUFFER_SIZE = 3500; //size of buffer to hold HTTP request
-
-// char request[IN_BUFFER_SIZE];
-// char response[OUT_BUFFER_SIZE]; //char array buffer to hold HTTP request
-// char brequest[IN_BUFFER_SIZE];
-// char bresponse[OUT_BUFFER_SIZE]; //char array buffer to hold HTTP request
-
-
-
-//         //auto-increasing value that goes from 0 to 4095 (approx) in steps of 100
-
-
-// //const char NETWORK[] = "MIT GUEST";
-// //const char PASSWORD[] = "";
-
-// const int BUTTON_TIMEOUT = 500; //button timeout in milliseconds
-
-// //
-// const char NETWORK[] = "MIT";
-// const char PASSWORD[] = "";
-
-// uint8_t channel = 1; //network channel on 2.4 GHz
-// byte bssid[] = {0x04, 0x95, 0xE6, 0xAE, 0xDB, 0x41}; //6 byte MAC address of AP you're targeting.
-
-// int wifi_object_builder(char* object_string, uint32_t os_len, uint8_t channel, int signal_strength, uint8_t* mac_address) {
-//   char temp[300];//300 likely long enough for one wifi entry
-//   int len = sprintf(temp, "{\"macAddress\": \"%02x:%02x:%02x:%02x:%02x:%02x\",\"signalStrength\": %d,\"age\": 0,\"channel\": %d}",
-//                  mac_address[0], mac_address[1], mac_address[2], mac_address[3], mac_address[4], mac_address[5],
-//                  signal_strength, channel);
-//   if (len>os_len){
-//     return 0;
-//   }else{
-//     return sprintf(object_string,"%s",temp);
-//   }
-// }
-
-
-// uint32_t timer;
-
-// void setup() {
-  
-//   Serial.begin(115200);               // Set up serial port
-
-//   //SET UP SCREEN:
-
-
-//   //PRINT OUT WIFI NETWORKS NEARBY
-//   int n = WiFi.scanNetworks();
-//   Serial.println("scan done");
-//   if (n == 0) {
-//     Serial.println("no networks found");
-//   } else {
-//     Serial.print(n);
-//     Serial.println(" networks found");
-//     for (int i = 0; i < n; ++i) {
-//       Serial.printf("%d: %s, Ch:%d (%ddBm) %s ", i + 1, WiFi.SSID(i).c_str(), WiFi.channel(i), WiFi.RSSI(i), WiFi.encryptionType(i) == WIFI_AUTH_OPEN ? "open" : "");
-//       uint8_t* cc = WiFi.BSSID(i);
-//       for (int k = 0; k < 6; k++) {
-//         Serial.print(*cc, HEX);
-//         if (k != 5) Serial.print(":");
-//         cc++;
-//       }
-//       Serial.println("");
-//     }
-//   }
-//   delay(100); //wait a bit (100 ms)
-
-//   //if using regular connection use line below:
-//   WiFi.begin(NETWORK, PASSWORD);
-//   //if using channel/mac specification for crowded bands use the following:
-//   //WiFi.begin(network, password, channel, bssid);
-
-//   uint8_t count = 0; //count used for Wifi check times
-//   Serial.print("Attempting to connect to ");
-//   Serial.println(NETWORK);
-//   while (WiFi.status() != WL_CONNECTED && count < 12) {
-//     delay(500);
-//     Serial.print(".");
-//     count++;
-//   }
-//   delay(2000);
-//   if (WiFi.isConnected()) { //if we connected then print our IP, Mac, and SSID we're on
-//     Serial.println("CONNECTED!");
-//     Serial.printf("%d:%d:%d:%d (%s) (%s)\n", WiFi.localIP()[3], WiFi.localIP()[2],
-//                   WiFi.localIP()[1], WiFi.localIP()[0],
-//                   WiFi.macAddress().c_str() , WiFi.SSID().c_str());
-//     delay(500);
-//   } else { //if we failed to connect just Try again.
-//     Serial.println("Failed to Connect :/  Going to restart");
-//     Serial.println(WiFi.status());
-//     ESP.restart(); // restart the ESP (proper way)
-//   }
-
-//   while (!Serial);
-//   delay(1000);
-//   unsigned status;
-//   status = bmp.begin();
-//   if (!status) {
-//     Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-//                      "try a different address!"));
-//     Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(), 16);
-//     esp_restart();
-//   }
-//   /* Default settings from datasheet. */
-//   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-//                   Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-//                   Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-//                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-//                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-//   Serial.println("setup done");
-//   timer = millis();
-  
-// }
-
-
-// //main body of code
-// void loop() {
-
-//   temperature = bmp.readTemperature()*1.8 +32;
-//   pressure = bmp.readPressure();
-//   altitude = bmp.readAltitude();
-  
-//   char num[] = "7";
-//   // sprintf(request, "GET http://608dev-2.net/sandbox/sc/team44/w1_sk_server.py?hello=%s HTTP/1.1\r\n", num);
-//   // strcat(request, "Host: 608dev-2.net\r\n"); //add more to the end
-//   // strcat(request, "\r\n"); //add blank line!
-//   // //submit to function that performs GET.  It will return output using response_buffer char array
-//   // do_http_request("608dev-2.net", request, response, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, true);
-  
-//   char body[100]; //for body
-//   sprintf(body,"user=%s&pressure=%f&altitude=%f&temperature=%f&hello=%s", USER, pressure, altitude, temperature,num);//generate body, posting to User, 1 step
-//   int body_len = strlen(body); //calculate body length (for header reporting)
-//   sprintf(brequest,"POST http://608dev-2.net/sandbox/sc/team44/w1_sk_server.py HTTP/1.1\r\n");
-//   strcat(brequest,"Host: 608dev-2.net\r\n");
-//   strcat(brequest,"Content-Type: application/x-www-form-urlencoded\r\n");
-//   sprintf(brequest+strlen(brequest),"Content-Length: %d\r\n", body_len); //append string formatted to end of request buffer
-//   strcat(brequest,"\r\n"); //new line from header to body
-//   strcat(brequest,body); //body
-//   strcat(brequest,"\r\n"); //new line
-//   do_http_request("608dev-2.net", brequest, bresponse, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT,true);
-
-//   Serial.println(response);
-//   Serial.println(bresponse);
-// }
-
-
 #include <SPI.h>
 #include <WiFi.h>
 #include <mpu6050_esp32.h>
@@ -178,6 +7,7 @@
 #include <WiFiClientSecure.h>
 #include <WiFiClient.h>
 #include <ArduinoJson.h>
+#include<math.h>
 
 WiFiClientSecure client; 
 WiFiClientSecure client2;
@@ -205,16 +35,34 @@ char json_body[JSON_BODY_SIZE];
 char wapirequest[IN_BUFFER_SIZE];
 char wapiresponse[OUT_BUFFER_SIZE]; //char array buffer to hold HTTP request
 char wapijson_body[JSON_BODY_SIZE];
+char request_buffer[IN_BUFFER_SIZE]; //char array buffer to hold HTTP request
+char response_buffer[OUT_BUFFER_SIZE]; //char array buffer to hold HTTP response
 
-const char city[]="cambridge";
-const char state[]="MA";
+MPU6050 imu; //imu object called, appropriately, imu
+float old_acc_mag;  //previous acc mag
+float older_acc_mag;  //previous prevoius acc mag
+bool hasBeenPosted = false;
+//some suggested variables you can use or delete:
+
+uint8_t state;  //system state for step counting
+uint8_t post_state; //state of posting
+int steps = 0;
+
+const uint8_t IDLE = 0;  //change if you'd like
+const uint8_t DOWN = 1;  //change if you'd like
+const uint8_t POST = 2;  //change if you'd like
+
 float altpressure = 1013.5;
 const char WEATHER_API_KEY[] = "";  // paste your key here
 const int MAX_APS = 5;
 
-
-        //auto-increasing value that goes from 0 to 4095 (approx) in steps of 100
-
+const uint8_t LOOP_PERIOD = 10; //milliseconds
+const int POSTING_PERIOD = 6000; //periodicity of getting a step.
+const int POSTING_PERIOD_SENSOR = 2000;
+uint32_t primary_timer = 0;
+uint32_t posting_timer = 0;
+uint32_t step_timer = 0;
+float x, y, z; //variables for grabbing x,y,and z values
 
 const char NETWORK[] = "MIT GUEST";
 const char PASSWORD[] = "";
@@ -316,6 +164,16 @@ void setup() {
   
   Serial.begin(115200);               // Set up serial port
 
+  Wire.begin();
+  delay(50); //pause to make sure comms get set up
+  if (imu.setupIMU(1)) {
+    Serial.println("IMU Connected!");
+  } else {
+    Serial.println("IMU Not Connected :/");
+    Serial.println("Restarting");
+    ESP.restart(); // restart the ESP (proper way)
+  }
+
   //PRINT OUT WIFI NETWORKS NEARBY
   int n = WiFi.scanNetworks();
   Serial.println("scan done");
@@ -381,14 +239,72 @@ void setup() {
                   Adafruit_BMP280::STANDBY_MS_500); 
   Serial.println("setup done");
   timer = millis();
-
   elevation = 8.0;
+  state = 0; //
+  post_state = IDLE; //initialize to something!
   
 }
 
 float alty = 0;
+
+//Post reporting state machine
+//use post_state for your state variable!
+void post_reporter_fsm() {
+  //your code here
+if(millis() - posting_timer > POSTING_PERIOD) {
+      char body[100]; //for body
+      sprintf(body,"user=%s&steps=%d",USER,steps);//generate body, posting to User, 1 step
+      int body_len = strlen(body); //calculate body length (for header reporting)
+      sprintf(request_buffer,"POST http://608dev-2.net/sandbox/sc/team44/steps.py HTTP/1.1\r\n");
+      strcat(request_buffer,"Host: 608dev.net\r\n");
+      strcat(request_buffer,"Content-Type: application/x-www-form-urlencoded\r\n");
+      sprintf(request_buffer+strlen(request_buffer),"Content-Length: %d\r\n", body_len); //append string formatted to end of request buffer
+      strcat(request_buffer,"\r\n"); //new line from header to body
+      strcat(request_buffer,body); //body
+      strcat(request_buffer,"\r\n"); //new line
+      Serial.println(request_buffer);
+      do_http_request("608dev-2.net", request_buffer, response_buffer, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT,true);
+      Serial.println(response_buffer); //viewable in Serial Terminal
+      //LCD Display:
+      char output[80];
+      posting_timer = millis();
+      // steps = 0;
+      hasBeenPosted = true;
+  }
+}
+
+//Step Counting FSM from Lab02A.  (bring over and adapt global variables as needed!)
+void step_reporter_fsm(float avg_acc_mag) {
+  //your code here
+  if(avg_acc_mag < 13.5/9.81 && avg_acc_mag > 12.5/9.81 && avg_acc_mag > older_acc_mag)
+    {
+      steps++;
+    }
+
+}
+
 //main body of code
 void loop() {
+
+  //get IMU information:
+  imu.readAccelData(imu.accelCount);
+  float x, y, z;
+  x = imu.accelCount[0] * imu.aRes;
+  y = imu.accelCount[1] * imu.aRes;
+  z = imu.accelCount[2] * imu.aRes;
+  float acc_mag = sqrt(x * x + y * y + z * z);
+  float avg_acc_mag = 1.0 / 3.0 * (acc_mag + old_acc_mag + older_acc_mag);
+  older_acc_mag = old_acc_mag;
+  old_acc_mag = acc_mag;
+
+  //get button readings:
+
+  step_reporter_fsm(avg_acc_mag); //run step_reporter_fsm (from lab02a)
+  post_reporter_fsm(); //run post_reporter_fsm (written here)
+
+  while (millis() - primary_timer < LOOP_PERIOD); //wait for primary timer to increment
+  // if statement
+  primary_timer = millis();
 
   temperature = bmp.readTemperature()*1.8 +32;
   pressure = bmp.readPressure();
@@ -429,7 +345,6 @@ void loop() {
   }
   
   alt_mag = altitude;
-  Serial.print(altitude);
   avg_alt_mag = (old_alt_mag + older_alt_mag)/2;
   delta = (alt_mag - avg_alt_mag);
   if (delta > 1){
@@ -456,20 +371,22 @@ void loop() {
   // strcat(request, "\r\n"); //add blank line!
   // //submit to function that performs GET.  It will return output using response_buffer char array
   // do_http_request("608dev-2.net", request, response, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, true);
-  
-  char body[100]; //for body
-  sprintf(body,"user=%s&pressure=%f&altitude=%f&temperature=%f&hello=%s", USER, pressure, elevation, temperature,num);//generate body, posting to User, 1 step
-  int body_len = strlen(body); //calculate body length (for header reporting)
-  sprintf(brequest,"POST http://608dev-2.net/sandbox/sc/team44/w1_sk_server.py HTTP/1.1\r\n");
-  strcat(brequest,"Host: 608dev-2.net\r\n");
-  strcat(brequest,"Content-Type: application/x-www-form-urlencoded\r\n");
-  sprintf(brequest+strlen(brequest),"Content-Length: %d\r\n", body_len); //append string formatted to end of request buffer
-  strcat(brequest,"\r\n"); //new line from header to body
-  strcat(brequest,body); //body
-  strcat(brequest,"\r\n"); //new line
-  do_http_request("608dev-2.net", brequest, bresponse, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT,true);
 
-  Serial.println(elevation);
+  if(millis() - posting_timer > POSTING_PERIOD_SENSOR) {
+    char body[100]; //for body
+    sprintf(body,"user=%s&pressure=%f&altitude=%f&temperature=%f&hello=%s", USER, pressure, elevation, temperature,num);//generate body, posting to User, 1 step
+    int body_len = strlen(body); //calculate body length (for header reporting)
+    sprintf(brequest,"POST http://608dev-2.net/sandbox/sc/team44/w1_sk_server.py HTTP/1.1\r\n");
+    strcat(brequest,"Host: 608dev-2.net\r\n");
+    strcat(brequest,"Content-Type: application/x-www-form-urlencoded\r\n");
+    sprintf(brequest+strlen(brequest),"Content-Length: %d\r\n", body_len); //append string formatted to end of request buffer
+    strcat(brequest,"\r\n"); //new line from header to body
+    strcat(brequest,body); //body
+    strcat(brequest,"\r\n"); //new line
+    do_http_request("608dev-2.net", brequest, bresponse, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT,true);
+  }
+
+  Serial.println(steps);
 }
 
 
