e699157e9f7a1a3ffe0fa37bbd8430c6a9cd6751
Javier Solis <javsolis@mit.edu>
2022-05-08 18:38:45 -0400

    updates to serve

--- server/map/bad_loc_table.py
+++ server/map/bad_loc_table.py
@@ -9,9 +9,8 @@ from main import make_html_table
 
 
 def request_handler(request) -> str:
-    c.execute('''CREATE TABLE IF NOT EXISTS bad_loc_data (lat real, lon real)''') # this exists just for curiosity purposes. May be useful later on too.
-
     with sqlite3.connect(current_db) as c:
+        c.execute('''CREATE TABLE IF NOT EXISTS bad_loc_data (lat real, lon real)''') # this exists just for curiosity purposes. May be useful later on too.
         allRows = c.execute('''SELECT * FROM bad_loc_data''').fetchall()
 
     html_render = make_html_table(("Latitude", "Longitude"), allRows)
